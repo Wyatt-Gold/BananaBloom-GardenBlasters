62bc14aa4ca5b50583d09cf0542eb50b7634f2f9
Wyatt-Gold <103477172+Wyatt-Gold@users.noreply.github.com>
2025-06-26 11:49:58 -0500

    Made Shooting better
    the gun rotates arounf the player, the gun flips when on differnt sides of player, the bullets are a better size and shoot from end of gun. Still need to fix bullet animation.

--- Assets/Scripts/Player/PlayerAiming.cs
+++ Assets/Scripts/Player/PlayerAiming.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using System;
 
 public class PlayerAiming : MonoBehaviour
 {
     public Transform firePoint;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = (mousePos - transform.position).normalized;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        firePoint.rotation = Quaternion.Euler(0, 0, angle);
+        firePoint.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 localScale = firePoint.localScale;
+        localScale.y = Mathf.Abs(localScale.y);
+        if(angle > 90 || angle < -90){
+            localScale.y *= -1;
+        }
+        firePoint.localScale = localScale;
     }
 }
--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -10,7 +10,7 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && Time.time >= nextFireTime)
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextFireTime)
         {
             Shoot();
             nextFireTime = Time.time + fireRate;
