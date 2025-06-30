60b26bd59b2ec552ebae005ce1ce7ab6598aea6d
Wyatt-Gold <103477172+Wyatt-Gold@users.noreply.github.com>
2025-06-30 15:43:57 -0500

    Added Custom hand and player sprites
    I added in the player and hand sprites. I also fixed the bullet animation so now it works. I als fixed roatoin, so it fits better with the player. Need to add player walking animations and have the player turn different directions. Will probably create a new player sprite aswell. Not happy with current one

--- Assets/Scripts/Player/PlayerAiming.cs
+++ Assets/Scripts/Player/PlayerAiming.cs
@@ -4,20 +4,26 @@ using System;
 public class PlayerAiming : MonoBehaviour
 {
     public Transform firePoint;
+    public Transform gunPivot;
 
-    // Update is called once per frame
     void Update(){
+        // Get mouse position in world
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (mousePos - transform.position).normalized;
+        mousePos.z = 0f;
 
+        // Calculate direction
+        Vector2 direction = (mousePos - gunPivot.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        firePoint.eulerAngles = new Vector3(0, 0, angle);
 
-        Vector3 localScale = firePoint.localScale;
+        // Rotate the pivot point
+        gunPivot.rotation = Quaternion.Euler(0, 0, angle);
+
+        // Flip gun vertically if aiming backwards
+        Vector3 localScale = gunPivot.localScale;
         localScale.y = Mathf.Abs(localScale.y);
-        if(angle > 90 || angle < -90){
+        if (angle > 90 || angle < -90){
             localScale.y *= -1;
         }
-        firePoint.localScale = localScale;
+        gunPivot.localScale = localScale;
     }
 }
